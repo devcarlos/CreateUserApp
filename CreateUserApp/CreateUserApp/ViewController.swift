e4f57c0e8cd2703bd42f365f4d4df856c1acd7d4
Carlos Alcala <carlos.alcala@me.com>
2022-10-17 14:01:18 -0400

    Base Create App Codebase

---
+++ CreateUserApp/CreateUserApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreateUserApp
+//
+//  Created by Carlos Alcala on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var repasswordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
